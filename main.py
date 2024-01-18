5ca50fb1c2e630cd00d2ee1c2e2f57a9f23c561f
JT Raber <JMANN240raber@gmail.com>
2024-01-17 20:54:28 -0500

    added backend logging

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 import os
+import logging
+
 from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO
 from werkzeug.utils import secure_filename
-from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -15,16 +17,23 @@ def chat():
 	return render_template('chat.html')
 
 def sendUsers():
+	logging.info("Emitting updated users.")
+	logging.info(f"Users: {', '.join([user['username'] for user in users.values()])}")
 	socketio.emit('users', users)
 
 @socketio.on('connect')
 def connect(auth):
 	username = auth['username']
+
+	logging.info(f"{username} connected.")
+	logging.info(f"SID: {request.sid}")
+
 	users[request.sid] = {
 		'username': username,
 		'status': 'idle'
 	}
 
+	logging.info("Emitting.")
 	socketio.emit('message', {
 		'username': 'SERVER',
 		'time': datetime.now().isoformat(),
@@ -36,8 +45,10 @@ def connect(auth):
 @socketio.on('disconnect')
 def disconnect():
 	user = users[request.sid]
+	logging.info(f"{user['username']} disconnected.")
 	del users[request.sid]
 
+	logging.info("Emitting.")
 	socketio.emit('message', {
 		'username': 'SERVER',
 		'time': datetime.now().isoformat(),
@@ -48,12 +59,16 @@ def disconnect():
 
 @socketio.on('message')
 def message(data):
-	print(f"{data['username']}: {data['text']}")
+	logging.info("Received message.")
+	logging.info(f"{data['username']}: {data['text']}")
+	logging.info("Emitting.")
 	socketio.emit('message', data)
 
 @socketio.on('status')
 def status(data):
-	users[request.sid]['status'] = data['status']
+	user = users[request.sid]
+	logging.info(f"Received status update for {user['username']}.")
+	user['status'] = data['status']
 	sendUsers()
 
 def allowed_file(filename):
@@ -65,16 +80,18 @@ IMAGE_FOLDER = 'static/images'
 
 @app.route('/image', methods=['POST'])
 def image():
-	if request.method == 'POST':
-		if 'image' not in request.files:
-			return "Image not provided", 400
-		image = request.files['image']
-		if image.filename == '':
-			return "Image not provided", 400
-		if image and allowed_file(image.filename):
-			filename = secure_filename(image.filename)
-			image.save(os.path.join(IMAGE_FOLDER, filename))
-			return filename
+	logging.info("Got image upload.")
+	if 'image' not in request.files:
+		return "Image not provided", 400
+	image = request.files['image']
+	if image.filename == '':
+		return "Image not provided", 400
+	if image and allowed_file(image.filename):
+		filename = secure_filename(image.filename)
+		filepath = os.path.join(IMAGE_FOLDER, filename)
+		logging.info(f"Saving image to {filepath}")
+		image.save(filepath)
+		return filename
 
 
 if __name__ == '__main__':
